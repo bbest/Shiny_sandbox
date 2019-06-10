c44cc8c78a54d1ffb507d3737b567ff442f37135
cjlortie <chris@christopherlortie.info>
2019-06-09 19:33:42 -0700

    quick setup

---
+++ map.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(leaflet)
+
+
+
+#ui
+ui <- fluidPage(
+  leafletOutput("map")
+)
+
+#server
+server <- function(input, output, session) {
+  
+  points <- eventReactive(input$recalc, {
+    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+  }, ignoreNULL = FALSE)
+  
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.TonerLite,
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>%
+      addMarkers(data = points())
+  })
+}
+
+#generate app
+shinyApp(ui, server)

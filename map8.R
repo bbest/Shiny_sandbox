477fa7687bcecc14077d6fd8ded6590f0068db6e
cjlortie <chris@christopherlortie.info>
2019-06-10 18:47:53 -0700

    tidy

---
+++ map8.R
@@ -0,0 +1,35 @@
+library(shiny)
+library(leaflet)
+
+#ui
+ui <- fluidPage(
+  titlePanel(h3("Habitat use at Carrizo Plain National Monument, USA")),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("var", label = h4("Variable"),
+                  choices = list("presence" = "total",
+                                "shrub cover" = "shrub_cover",
+                                 "NDVI" = "NDVI",
+                                 "slope" = "slope",
+                                 "elevation" = "elevation",
+                                 "aspect" = "aspect",
+                                 "solar" = "solar"), selected = 1)
+    ),
+    mainPanel = leafletOutput("map")
+  )
+)
+
+
+#server
+server <- function(input, output, session) {
+  data <- read.csv("data/mcp20m.csv")
+  t.class <- colorFactor("Blues", data[,input$var], levels = TRUE)
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      addCircleMarkers(data$x_wgs, data$y_wgs, radius = 1, color = ifelse(data[,input$var] == "0", 'lightblue', 'red'))
+  })
+}
+
+#generate app
+shinyApp(ui, server)
